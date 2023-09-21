49606cb4a862c8f4c3ab23f600959f01b7a61aa7
Ethan Marchand <ethanjmarchand@gmail.com>
2023-09-21 12:04:36 -0600

    Ethan

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func homehandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<p>Welcome to my new site!</p>")
 }
 
-func contacthandler(w http.ResponseWriter, r *http.Request) {
+func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "text/html; charset=utf-8")
 	fmt.Fprint(w, `
 		<h1>Contact Page</h1>
@@ -18,20 +18,26 @@ func contacthandler(w http.ResponseWriter, r *http.Request) {
 	`)
 }
 
-func pathhandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homehandler(w, r)
-	case "/contact":
-		contacthandler(w, r)
-	default:
-		http.Error(w, "404 | Page not found", http.StatusNotFound)
-	}
+func faqhandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `
+		<h1>FAQ</h1>
+		<h2><Strong>Q.</strong> What is the best way to get ahold of you?</h2>
+		<p><strong>A.</strong> Phone!</p>
+		<hr>
+		<h2><Strong>Q.</strong> What is the best way to get ahold of you?</h2>
+		<p><strong>A.</strong> Phone!</p>
+		<hr>
+		<h2><Strong>Q.</strong> What is the best way to get ahold of you?</h2>
+		<p><strong>A.</strong> Phone!</p>
+		<hr>
+		<h2><Strong>Q.</strong> What is the best way to get ahold of you?</h2>
+		<p><strong>A.</strong> Phone!</p>
+
+	`)
 }
 
-// type Router struct{}
-
-// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// func pathhandler(w http.ResponseWriter, r *http.Request) {
 // 	switch r.URL.Path {
 // 	case "/":
 // 		homehandler(w, r)
@@ -42,8 +48,24 @@ func pathhandler(w http.ResponseWriter, r *http.Request) {
 // 	}
 // }
 
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homehandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	case "/faq":
+		faqhandler(w, r)
+	default:
+		http.Error(w, "404 | Page not found", http.StatusNotFound)
+	}
+}
+
 func main() {
+	var router Router
 	fmt.Println("starting the server on :3000...")
-	http.ListenAndServe(":3000", http.HandlerFunc(pathhandler))
+	http.ListenAndServe(":3000", router)
 
 }

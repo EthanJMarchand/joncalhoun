82615e881afee35b05ae9a03c1b0e163a295869f
Ethan Marchand <ethanjmarchand@gmail.com>
2023-09-21 07:30:33 -0600

    Ethan

--- main.go
+++ main.go
@@ -5,13 +5,37 @@ import (
 	"net/http"
 )
 
-func Handlerfunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<p>Hello world!</p>")
+func homehandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<p>Welcome to my new site!</p>")
+}
+
+func contacthandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `
+		<h1>Contact Page</h1>
+		<p>contact me at <a href="mailto:ethan@ethanmarchand.com">email me</a></p>
+	`)
+}
+
+// func pathhandler(w http.ResponseWriter, r *http.Request) {}
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homehandler(w, r)
+	case "/contact":
+		contacthandler(w, r)
+	default:
+		http.Error(w, "404 | Page not found", http.StatusNotFound)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", Handlerfunc)
+	var router Router
 	fmt.Println("starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 
 }

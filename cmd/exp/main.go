1746ea207cd2ed6ad534cb4368680784fae4945b
Ethan Marchand <ethanjmarchand@gmail.com>
2023-09-21 23:23:25 -0600

    Ethan

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"os"
+)
+
+type User struct {
+	Name string
+	Age  int
+	Meta UserMeta
+}
+
+type UserMeta struct {
+	Visits int
+}
 
 func main() {
-	fmt.Println("hello, world!")
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	// u1 := struct {
+	// 	Name string
+	// 	Age  int
+	// }{
+	// 	Name: "John Wayne",
+	// 	Age:  32,
+	// }
+
+	u1 := User{
+		Name: "Bruce Wayne",
+		Age:  37,
+		Meta: UserMeta{
+			Visits: 4,
+		},
+	}
+
+	err = t.Execute(os.Stdout, u1)
+	if err != nil {
+		panic(err)
+	}
 }

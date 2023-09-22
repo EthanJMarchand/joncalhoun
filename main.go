3cfd31efa518aed4d7321d62b611836b087361fb
Ethan Marchand <ethanjmarchand@gmail.com>
2023-09-21 21:52:40 -0600

    Ethan

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func homehandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<p>Welcome to my new site!!!/p>")
+	fmt.Fprint(w, "<p>Welcome to my new site!!!</p>")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
@@ -33,39 +35,18 @@ func faqhandler(w http.ResponseWriter, r *http.Request) {
 		<hr>
 		<h2><Strong>Q.</strong> What is the best way to get ahold of you?</h2>
 		<p><strong>A.</strong> Phone!</p>
-
 	`)
 }
 
-// func pathhandler(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homehandler(w, r)
-// 	case "/contact":
-// 		contacthandler(w, r)
-// 	default:
-// 		http.Error(w, "404 | Page not found", http.StatusNotFound)
-// 	}
-// }
-
-type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homehandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/faq":
-		faqhandler(w, r)
-	default:
-		http.Error(w, "404 | Page not found", http.StatusNotFound)
-	}
-}
-
 func main() {
-	var router Router
+	r := chi.NewRouter()
+	r.Get("/", homehandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqhandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "404 | Page not found", http.StatusNotFound)
+	})
 	fmt.Println("starting the server on :3000...")
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", r)
 
 }

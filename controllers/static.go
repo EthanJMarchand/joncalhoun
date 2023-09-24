3b4e5fb9f4d31a7d4d355903ec7f6ac6338bcac5
Ethan Marchand <ethanjmarchand@gmail.com>
2023-09-23 19:36:16 -0600

    Ethan

--- controllers/static.go
+++ controllers/static.go
@@ -1,13 +1,37 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/ethanjmarchand/joncalhoun/views"
 )
 
-func StaticHandler(tmpl views.Template) http.HandlerFunc {
+func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		tmpl.Execute(w, nil)
+		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "What is the best way to learn code?",
+			Answer:   "You have to start coding!",
+		},
+		{
+			Question: "What are the three rules of real estate?",
+			Answer:   "Location, Location, Location",
+		},
+		{
+			Question: "What is the average air speed velocity of a swallow?",
+			Answer:   "African, or American?",
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
 	}
 }

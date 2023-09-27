c392bbd401d7eb00ce6f94f4ec69f88016bda654
Ethan Marchand <ethanjmarchand@gmail.com>
2023-09-27 13:03:48 -0600

    Ethan

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -7,8 +7,29 @@ import (
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
+type postgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Dbname   string
+	SSLMode  string
+}
+
+func (cfg postgresConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Dbname, cfg.SSLMode)
+}
+
 func main() {
-	db, err := sql.Open("pgx", "host=localhost port=5432 user=baloo password=junglebook dbname=jon_calhoun sslmode=disable")
+	cfg := postgresConfig{
+		Host:     "localhost",
+		Port:     "5432",
+		User:     "baloo",
+		Password: "junglebook",
+		Dbname:   "jon_calhoun",
+		SSLMode:  "disable",
+	}
+	db, err := sql.Open("pgx", cfg.String())
 	if err != nil {
 		panic(err)
 	}
@@ -18,5 +39,103 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("connected")
+	fmt.Println("Connected")
+
+	// Create a table ...
+
+	_, err = db.Exec(`
+	CREATE TABLE IF NOT EXISTS users (
+		id SERIAL PRIMARY KEY,
+		name TEXT, 
+		email TEXT UNIQUE NOT NULL
+	);
+
+	CREATE TABLE IF NOT EXISTS orders (
+		id SERIAL PRIMARY KEY,
+		user_id INT NOT NULL,
+		amount INT,
+		description TEXT
+	);
+	`)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Table's created.")
+
+	// Insert some data...
+	// name := "Jon Smith"
+	// email := "jon@clhoun.io"
+	// row := db.QueryRow(`
+	// 	INSERT INTO users (name, email)
+	// 	VALUES ($1, $2)
+	// 	RETURNING id;`, name, email)
+	// var id int
+	// err = row.Scan(&id)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("User created. id = ", id)
+
+	// Query a single record
+	// id := 3
+	// row := db.QueryRow(`
+	// 	SELECT name, email
+	// 	FROM users
+	// 	WHERE id=$1;`, id)
+	// var name, email string
+	// err = row.Scan(&name, &email)
+	// if err == sql.ErrNoRows {
+	// 	fmt.Println("Error, no rows!")
+	// }
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Printf("User's information: name=%s, email=%s", name, email)
+
+	// userID := 1
+	// for i := 1; i <= 5; i++ {
+	// 	amount := i * 37
+	// 	desc := fmt.Sprintf("Fake order #%d", i)
+	// 	_, err := db.Exec(`
+	// 	INSERT INTO orders(user_id, amount, description)
+	// 	VALUES($1, $2, $3);`, userID, amount, desc)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// }
+	// fmt.Println("Created fake order")
+
+	type Order struct {
+		ID          int
+		UserID      int
+		Amount      int
+		Description string
+	}
+
+	var orders []Order
+	userID := 1
+	rows, err := db.Query(`
+		SELECT id, amount, description
+		FROM orders
+		WHERE user_id=$1;`, userID)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var order Order
+		order.UserID = userID
+		err := rows.Scan(&order.ID, &order.Amount, &order.Description)
+		if err != nil {
+			panic(err)
+		}
+		orders = append(orders, order)
+	}
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Orders:", orders)
 }
